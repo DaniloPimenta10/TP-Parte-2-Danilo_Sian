cb7a6d1a77847f396ac5f571339efcda7b0aa302
gabrielemodesto <bibiairam1@gmail.com>
2026-05-15 12:53:28 -0300

    atualização da função carrega_partidas

--- bd_partidas.c
+++ bd_partidas.c
@@ -14,8 +14,8 @@ void carrega_partidas(BDPartidas *bdp, BDTimes *bdt, char *caminho) {
     //pula a primeira linha
     fgets(buffer, sizeof(buffer), f);
 
-    //continua lendo linha por linh ate o final do arquivo ou ate a linha 10
-    while (fgets(buffer, sizeof(buffer), f))
+    
+    while (fgets(buffer, sizeof(buffer), f)) 
     {
         if (sscanf(buffer, "%d,%d,%d,%d,%d" , // Checa se a linha com os times esta correta
             &bdp->partida[bdp->qtd].id,      // Se estiver correta atribui um valor as variaveis
@@ -24,6 +24,27 @@ void carrega_partidas(BDPartidas *bdp, BDTimes *bdt, char *caminho) {
             &bdp->partida[bdp->qtd].gols1,
             &bdp->partida[bdp->qtd].gols2) == 5)
         {
+            Partida p = bdp->partidas[bdp->qtd]; // nao entendi essa linha
+            Time *t1 = &bdt->times[p.idTime1];
+            Time *t2 = &bdt->times[p.idTime2];
+            t1->gm += p.gols1;
+            t1->gs += p.gols2;
+            t2->gm += p.gols2;
+            t2->gs += p.gols1;
+
+            if (p.gols1 > p.gols2) {
+                t1->v += 1;
+                t2->d += 1;
+            } else if (p.gols1 < p.gols2)
+            {
+                t2->v += 1;
+                t1->d += 1;
+            }
+            else {
+                t1->e += 1;
+                t2->e += 1;
+            }
+            
             bdp->qtd++;
         }
     }
